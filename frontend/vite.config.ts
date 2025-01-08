b87ebac10c191c38cab66b8c3d50c3370c440f07
Hassan <hassanhadi@gmx.de>
2025-01-08 19:10:25 +0100

    adding hot reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		watch: {
+			usePolling: true
+		},
+		host: true,
+		strictPort: true,
+		port: 3000
+	},
 	plugins: [sveltekit()]
 });
